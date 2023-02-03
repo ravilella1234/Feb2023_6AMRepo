e9080eceb5d8c93c33d54ac04a269576a321902f
ravilella1234 <masters.selenium@gmail.com>
2023-02-03 06:57:17 +0530

    Abstract Class Modifiers

---
+++ Feb2023JavaBatch/src/icici/loans/homeloans/C.java
@@ -0,0 +1,22 @@
+package icici.loans.homeloans;
+
+import icici.loans.carloans.A;
+
+public class C extends A
+{
+	public void m3()
+	{
+		System.out.println("iam m3 from C");
+	}
+	
+	public static void main(String[] args) 
+	{
+		A a = new A();
+		a.m1();
+		
+		C c = new C();
+		c.m3();
+		c.m1();
+	}
+
+}
