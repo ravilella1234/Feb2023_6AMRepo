e43992f2e2b7bca38dcf7ff06abfccf3352b8908
ravilella1234 <masters.selenium@gmail.com>
2023-03-04 06:58:29 +0530

    Properties Files

--- March2023_6AM_SeleniumBatch/src/test/java/selenium/BaseTest.java
+++ March2023_6AM_SeleniumBatch/src/test/java/selenium/BaseTest.java
@@ -1,5 +1,8 @@
 package selenium;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,16 +13,42 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest 
 {
 	public static WebDriver driver;
+	public static String ProjectRootPath = System.getProperty("user.dir")+"/src/test/resources";
+	public static FileInputStream fis;
+	public static Properties p;
+	public static Properties mainProp;
+	public static Properties childProp;
+	
+	public static void init() throws Exception
+	{
+		System.out.println(System.getProperty("user.dir"));
+		//FileInputStream fis = new FileInputStream("D:\\April2022WD\\March2023_6AM_SeleniumBatch\\src\\test\\resources\\data.properties");
+		fis = new FileInputStream(ProjectRootPath+"/data.properties");
+		p = new Properties();
+		p.load(fis);
+		
+		fis = new FileInputStream(ProjectRootPath+"/environment.properties");
+		mainProp = new Properties();
+		mainProp.load(fis);
+		String e = mainProp.getProperty("env");
+		System.out.println(e);
+		
+		fis = new FileInputStream(ProjectRootPath+"/"+e+".properties");
+		childProp = new Properties();
+		childProp.load(fis);
+		System.out.println(childProp.getProperty("amazonurl"));
+		
+	}
 	
 	public static void launch(String browser)
 	{
-		if(browser.equals("chrome")) {
+		if(p.getProperty(browser).equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-		}else if(browser.equals("firefox")) {
+		}else if(p.getProperty(browser).equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-		}else if(browser.equals("ie")) {
+		}else if(p.getProperty(browser).equals("ie")) {
 			WebDriverManager.iedriver().setup();
 			driver = new InternetExplorerDriver();
 		}
@@ -27,7 +56,7 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(url);
+		driver.get(childProp.getProperty(url));
 	}
 
 }
--- March2023_6AM_SeleniumBatch/src/test/java/selenium/TC_001.java
+++ March2023_6AM_SeleniumBatch/src/test/java/selenium/TC_001.java
@@ -3,8 +3,10 @@ package selenium;
 public class TC_001 extends BaseTest
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		init();
+		
 		launch("chromebrowser");
 		
 		navigateUrl("amazonurl");
